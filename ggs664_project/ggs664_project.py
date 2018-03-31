107c5e14851063d23739231983f5b50c7abee1fe
Chris Smith <smitty1egh@github.com>
2018-03-31 17:20:01 +0100

    gephi output in progress

--- ggs664_project/ggs664_project.py
+++ ggs664_project/ggs664_project.py
@@ -1,52 +1,38 @@
 from   datetime          import datetime
 import logging
 import logging.config
-logging.config.fileConfig('logging.conf',disable_existing_loggers=False)
-logger=logging.getLogger('sqlalchemy').setLevel(logging.DEBUG)
-logger=logging.getLogger('root')
-
 import pickle
-#import sqlite3
 
+import numpy
+from   scipy.spatial     import ConvexHull
 import shapefile
 from   sqlalchemy        import create_engine
 from   sqlalchemy.orm    import sessionmaker
 from   sqlalchemy.sql    import text
+
 from   model             import run__init__,District,DistrictAdjacency,Run,OutputDetail
 
+logging.config.fileConfig('logging.conf',disable_existing_loggers=False)
+logger=logging.getLogger( 'sqlalchemy').setLevel(logging.DEBUG)
+logger=logging.getLogger( 'root')
+
 SQLFILE  ='sqlite:///ggs664.sqlite'
+COUNTIES ="/mnt/swap/VirgiVVnia_Administrative_Boundary_2017_SHP/VA_COUNTY"
+DISTRICTS="/mnt/swap/tl_2012_51_vtd10/tl_2012_51_vtd10"
+LOAD_DATA=True #False #
+EXPORTDAT=True #False #
+MAX_SEED =11
+
 engine   = create_engine(SQLFILE, echo=True)
 session  = sessionmaker(bind=engine)()
 conn     = engine.connect()
 rawconn  = engine.raw_connection()
 
-COUNTIES ="/mnt/swap/Virginia_Administrative_Boundary_2017_SHP/VA_COUNTY"
-DISTRICTS="/mnt/swap/tl_2012_51_vtd10/tl_2012_51_vtd10"
-LOAD_DATA=False #True #
-MAX_SEED =11
-
-def shapelist2set(pointstring):
-    '''POINTSTRING is a CSV list of LON/LAT pairs.
-    '''
-    def genpoints(pointstring):
-        '''Return a generator to pull the point string values out of the source data.
-           First, replace the point commas with pipes and then split on pipe.
-             We do this so that we can split the point pairs while preserving
-               the internal comma within the point.
-             Then, drop the parens and split on the internal comma.
-             Finally, make tuples of floats, if needed.
-        '''
-        x=pointstring.replace('[','').replace(']','').replace('), ',')|').split('|')
-        for y in x:
-            z=y.replace('(','').replace(')','').split(', ')
-            yield tuple(( float(z[0])
-                        , float(z[1])
-                       ))
-    ps=set()
-    for x in genpoints(pointstring):
-        ps.add(x)
-    return ps
-
+GLO_PERM = 0  #Float meaning the perimiter
+GLO_VERT = 1  #[(Float,Float),] points
+gloseeds = {} #Golbal dictionary that will be called from SQLite to remember the
+              #  current perimiter and vertices of the districts, so that the
+              #  one with optimal characteristics can be merged.
 
 def load_data():
     '''We ETL data from the shapefile, merging the metadata with the
@@ -57,6 +43,35 @@ def load_data():
        When NOT disjoint, we deem them neighbors, and store that fact
          in the district_adjacency table.
     '''
+    def shapelist2set(pointstring):
+        '''POINTSTRING is a CSV list of LON/LAT pairs.
+        '''
+        def genpoints(pointstring):
+            '''Return a generator to pull the point string values out of the source data.
+               First, replace the point commas with pipes and then split on pipe.
+                 We do this so that we can split the point pairs while preserving
+                   the internal comma within the point.
+                 Then, drop the parens and split on the internal comma.
+                 Finally, make tuples of floats, if needed.
+            '''
+            x=pointstring.replace('[','').replace(']','').replace('), ',')|').split('|')
+            for y in x:
+                z=y.replace('(','').replace(')','').split(', ')
+                yield tuple(( float(z[0])
+                            , float(z[1])
+                           ))
+        ps=set()
+        for x in genpoints(pointstring):
+            ps.add(x)
+        return ps
+
+    def shapelist_perim(spoints):
+        '''Take in a set point shape points and calculate the convex hull.
+        '''
+        ch=ConvexHull(numpy.array(list(spoints)))
+        return ch.area #for 2D, area==perimiter
+
+
     SQL_ADJ  ='''SELECT C.a_district_id, C.a_shape_points
                       , C.b_district_id, C.b_shape_points
                  FROM  (SELECT A.district_id  as a_district_id
@@ -68,6 +83,7 @@ def load_data():
                  WHERE  C.a_district_id != C.b_district_id;
               '''
     for d in shapefile.Reader(DISTRICTS).shapeRecords():
+        spoints=shapelist2set(str(d.shape.points))
         dd=District(STATEFP10   =d.record[0] ,COUNTYFP10=d.record[1]
                    ,VTDST10     =d.record[2] ,GEOID10   =d.record[3]
                    ,VTDI10      =d.record[4] ,NAME10    =d.record[5]
@@ -75,7 +91,8 @@ def load_data():
                    ,MTFCC10     =d.record[8] ,FUNCSTAT10=d.record[9]
                    ,ALAND10     =d.record[10],AWATER10  =d.record[11]
                    ,INTPTLAT10  =d.record[12],INTPTLON10=d.record[13]
-                   ,shape_points=shapelist2set(str(d.shape.points))
+                   ,shape_points=spoints
+                   ,shape_perim =shapelist_perim(spoints)
                    )
         session.add(dd)
     session.commit()
@@ -94,13 +111,11 @@ def load_data():
                 session.add(da)
     session.commit()
 
-
-
 def do_runs(runs :int):
     '''For each run, start with the 11 districts, there is a 'seed' entry.
        Load all of the seed entries, and add them to output_detail.
     '''
-    SQL_NOM  ='''INSERT INTO congdistrictseed(district_id)
+    SQL_NOM  ='''INSERT INTO seed_district(district_id)
                  SELECT      district_id
                  FROM        districts
                  ORDER BY    random()
@@ -109,34 +124,34 @@ def do_runs(runs :int):
     SQL_NEXT_ADJ='''
         SELECT    district_right_id               as next_adjacency
                , (SELECT district_id
-                  FROM   congdistrictseed
-                  WHERE  cong_district_seed_id=%s) as congdistrictseed
-        FROM      districtadjacency
+                  FROM   seed_district
+                  WHERE  seed_district_id=%s)      as congdistrictseed
+        FROM      district_adjacency
         WHERE     district_left_id
-              IN (SELECT  district_id             as superdistrict
-                  FROM    outputdetail
-                  WHERE   run_id                          = %s
-                     AND  cong_district_seed_id           =
-                            (SELECT district_id
-                             FROM   congdistrictseed
-                             WHERE  cong_district_seed_id = %s ))
+              IN (SELECT district_id               as superdistrict
+                  FROM   output_detail
+                  WHERE  run_id                     = %s
+                     AND seed_district_id           =
+                           (SELECT district_id
+                            FROM   seed_district
+                            WHERE  seed_district_id = %s ))
              AND  district_right_id
-          NOT IN (SELECT  district_id           as already_used
-                  FROM    outputdetail
+          NOT IN (SELECT  district_id              as already_used
+                  FROM    output_detail
                   WHERE   run_id                          = %s)
         ORDER BY  RANDOM()
         LIMIT     1;
     '''
     SQL_INS_OUTP='''
-        INSERT INTO outputdetail(run_id,step,cong_district_seed_id,district_id)
+        INSERT INTO output_detail(run_id,step,seed_district_id,district_id)
         VALUES     (?,?,?,?);
     '''
-    conn.execute(text("DELETE FROM outputdetail;"))
+    conn.execute(text("DELETE FROM output_detail;"))
     session.commit()
 
     for r in range(runs):
         #Nominate 11 seed CongDistricts
-        conn.execute(text("DELETE FROM congdistrictseed;"))
+        conn.execute(text("DELETE FROM seed_district;"))
         session.commit()
         conn.execute(SQL_NOM)
         session.commit()
@@ -165,4 +180,5 @@ def do_runs(runs :int):
 
 if __name__=='__main__':
     if LOAD_DATA: load_data()
-    do_runs(100)
+    if EXPORTDAT: export_dat()
+    do_runs(10)
--- ggs664_project/model.py
+++ ggs664_project/model.py
@@ -2,7 +2,7 @@ from datetime                   import datetime
 from typing                     import *
 
 from sqlalchemy                 import create_engine
-from sqlalchemy                 import Column, Integer, String, DateTime, PickleType
+from sqlalchemy                 import Column, Integer, Float, String, DateTime, PickleType
 from sqlalchemy.ext.declarative import declarative_base
 
 Base=declarative_base()
@@ -11,20 +11,26 @@ def run__init__(conn :Any, session :Any, step :int)->int:
     '''Make a new run, entry, seed the outputdetail tablename
          with a self-referential entry so that the 'used' query doesn't
          return a null.
-       Use SEEDS to make sure that the cong_district_seed_id is meaningful.
+       Use SEEDS to make sure that the seed_district_id is meaningful.
     '''
     SQL_NEW_RUN='''
-       insert into outputdetail(run_id,step,cong_district_seed_id,district_id)
+       insert into output_detail(run_id,step,seed_district_id,district_id)
        select      ?, ?, district_id, district_id
-       from        congdistrictseed;
+       from        seed_district;
     '''
     r=Run(run_start=datetime.now()); session.add(r); session.commit()
     conn.execute(SQL_NEW_RUN,(r.run_id,step))
     session.commit()
     return r.run_id
 
+def compute_perimiter(seed_district_id :Any, adjacent_district_id :Any):
+    '''Take in two pickled point sets;
+    '''
+
 class District(Base):
     '''Based on TIGER tl_2012_51_vtd10
+       These are the ~2,300 voting districts that roll up into their
+         11 Congressional districts for Virginia.
     '''
     __tablename__='districts'
     district_id  = Column(Integer, primary_key=True)
@@ -42,29 +48,44 @@ class District(Base):
     AWATER10     = Column(String)
     INTPTLAT10   = Column(String)
     INTPTLON10   = Column(String)
-    shape_points = Column(PickleType)
+    shape_points = Column(PickleType) #pickled set()
+    shape_perim  = Column(Float)
 
 class DistrictAdjacency(Base):
     '''Which districts have shape_points that are NOT disjoint.
     '''
-    __tablename__     ='districtadjacency'
+    __tablename__     ='district_adjacency'
     district_left_id  = Column(Integer, primary_key=True)
     district_right_id = Column(Integer, primary_key=True)
 
-class CongDistrictSeed(Base):
+    def dump(f):
+        '''F is an open file handle.
+           Dump table in XML format.
+        '''
+        FRAG0='''<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+    <meta lastmodifieddate="2009-03-20">
+        <creator>Gexf.net</creator>
+        <description>District Adjacency Dump</description>
+    </meta>
+    <graph mode="static" defaultedgetype="directed">
+        <nodes>
+        '''
+        FRAG1='</nodes><edges>'
+        FRAG2='</edges></graph></gexf>'
+        #SELECT '<node id="' ||  || '" label="' ||  || '"/>' FROM INNER JOIN ON;
+        #SELECT '<edge id="' ||  || '" source="' ||  || '" target="' ||  || '" />' FROM INNER JOIN ON;
+
+
+class SeedDistrict(Base):
     '''These are districts that are selected manually as them
          basis for building congressional districts.
        This lets the calculation start with some initial dispersion,
          so that we don't wind up choking off the corners.
-    INSERT INTO congdistrictseed(district_id)
-    SELECT      district_id
-    FROM        districts
-    ORDER BY    random()
-    LIMIT 11;
     '''
-    __tablename__         ='congdistrictseed'
-    cong_district_seed_id = Column(Integer, primary_key=True)
-    district_id           = Column(Integer)
+    __tablename__    ='seed_district'
+    seed_district_id = Column(Integer, primary_key=True)
+    district_id      = Column(Integer)
 
 class Run(Base):
     '''We manage the run duration and get a key here.
@@ -80,30 +101,13 @@ class OutputDetail(Base):
        We will randomly pick a voting distcrict
          that is adjacent to the merged districts from the OutputDetail,
          but has not already been taken.
-
-       INSERT INTO outputdetail( run_id
-                               , step
-                               , cong_district_seed_id
-                               , district_id
-                               )
-       SELECT      ?, ?, ?, district_right_id
-       FROM        districtadjacency
-       WHERE       district_left_id IN      (SELECT district_id
-                                             FROM   outputdetail
-                                             WHERE  run_id                 = ?
-                                                AND cong_distcrict_seed_id = ?)
-               AND district_right_id NOT IN (SELECT district_id
-                                             FROM   outputdetail
-                                             WERE   run_id                 = ?)
-       ORDER BY    RANDOM()
-       LIMIT 1;
     '''
-    __tablename__         ='outputdetail'
-    output_detail_id      = Column(Integer, primary_key=True)
-    run_id                = Column(Integer)
-    step                  = Column(Integer)
-    cong_district_seed_id = Column(Integer)
-    district_id           = Column(Integer)
+    __tablename__    ='output_detail'
+    output_detail_id = Column(Integer, primary_key=True)
+    run_id           = Column(Integer)
+    step             = Column(Integer)
+    seed_district_id = Column(Integer)
+    district_id      = Column(Integer)
 
 
 if __name__=='__main__':
